56ce3607a614b3bb4048bee1221f326f094427c0
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2019-03-04 12:45:39 +0200

    cscope plugin and git ignore global
    - added cscope basic config
    - added `.gitignore_global`
    - added symlink creation to `setup.sh` for global git ignore

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ ln -s $HOME/.vim/.vimrc $HOME/.vimrc
 echo 'Making link for `.ctags`'
 ln -s $HOME/.vim/.ctags $HOME/.ctags
 
+echo 'Making link for `.gitignore_global`'
+ln -s $HOME/.vim/.gitignore_global $HOME/.gitignore_global
+
 echo 'Run vim to install all plugins'
 vim +PluginInstall +qall
 echo 'Done.'
