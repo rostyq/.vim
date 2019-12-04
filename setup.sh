18f5fd2dc83fa0d87a815cc68ea25907f5dd5d4a
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2019-12-05 00:32:47 +0200

    update setup sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
+SCRIPT=$(dirname "$0")
+
 echo 'Configuring vim script'
 echo 'Getting vundle'
-git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/vundle.git $SCRIPT/bundle/Vundle.vim
 
 echo 'Making link for `.vimrc`'
-ln -s $HOME/.vim/.vimrc $HOME/.vimrc
+ln -s $SCRIPT/.vimrc $HOME/.vimrc
 
 echo 'Making link for `.ctags`'
-ln -s $HOME/.vim/.ctags $HOME/.ctags
+ln -s $SCRIPT/.ctags $HOME/.ctags
 
 echo 'Making link for `.gitignore_global`'
-ln -s $HOME/.vim/.gitignore_global $HOME/.gitignore_global
+ln -s $SCRIPT/.gitignore_global $HOME/.gitignore_global
+
+echo 'Creating vim directories'
+mkdir $SCRIPT/.backup
+mkdir $SCRIPT/.swap
+mkdir $SCRIPT/.undo
+mkdir $SCRIPT/view
 
 echo 'Run vim to install all plugins'
 vim +PluginInstall +qall
